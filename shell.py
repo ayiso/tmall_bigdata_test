43a7552a3713cc28ea571ef34e9d41d5be2f974c
yao shi <yaoshi@yaodeMac-mini.local>
2017-03-26 15:35:57 +0800

    add shell.py

---
+++ shell.py
@@ -0,0 +1,18 @@
+
+import mall as x
+x.format_item_csv()
+x.split_item_csv()
+x.split_user_csv()
+x.user_data_clean()
+
+x.save_item_sell_to_cateid_csv()
+
+x.clean_test_data()
+x.get_test_day_data(31)
+
+calc_list = x.calculate_item(1, 31, 25, 30, 30, 20, 1, 35)
+#x.calculate_item(0, 30, 25, 30, 30, 20, 1, 35)#test mode 1, normal mode 0
+
+real_list = x.get_result_day_buy_list(1) #1 item, other cate
+
+x.f1_result(calc_list,real_list)
